9dfcecf4d5d989359942d80a7974b4867602a51b
Dean Scelza and Nhat Hoang <nhat@>
2016-08-24 10:26:54 -0700

    hold user input to a variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
-$(document).ready(function() {
-  $("form").submit(function(event) {
+function Player1(player) {
+  this.playerName = player;
+  this.score = 0;
+};
 
-    event.preventDefault();
-  })
-})
+function Player2(player) {
+  this.playerName = player;
+  this.score = 0;
+};
+
+var diceRoll = function() {
+   return Math.floor((Math.random() * 6) + 1);
+ };
+//
+//  Player.prototype.increaseScore = function(diceRoll) {
+//    this.score += diceRoll;
+// };
+
+
+
+$(document).ready(function() {
+  $("#pigForm").submit(function(event) {
+      event.preventDefault();
+
+      var playerName1 = $("input#playerName1").val();
+      var playerName2 = $("input#playerName2").val();
+
+
+    var nameHolder1 = new Player1(playerName1);
+    var nameHolder2 = new Player2(playerName2);
+
+
+  });
+});
