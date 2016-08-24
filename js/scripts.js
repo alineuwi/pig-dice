16a6a4dca2a0e2091d05a774b11e2e16c27c80cf
Dean Scelza and Nhat Hoang <dean@>
2016-08-24 11:36:36 -0700

    create functions to roll die, hold score, switch players & reset game

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+var pigGame = {
+  player1Score: 0,
+  player2Score: 0,
+  playerUp: 1,
+  turnScore: 0,
+};
+
+function dieRoll () {
+  die1 = Math.floor(Math.random()*6) +1;
+  return die1;
+}
+
+var playerRoll = function() {
+  var roll = dieRoll();
+  if(roll ===1){
+    pigGame.turnScore = 0;
+    notifyEndTurn();
+    switchPlayer();
+  } else {
+    pigGame.turnScore +=roll;
+    if (pigGame.playerUp === 1) {
+      if (pigGame.turnScore + pigGame.player1Score >= 50) {
+        alertWinner(1);
+      }
+    } else if (pigGame.turnScore + pigGame.player2Score >= 50) {
+      alertWinner(2);
+  }
+  }
+  return roll;
+}
+
+
+
+
+
 function Player1(player) {
   this.playerName = player;
   this.score = 0;
@@ -7,19 +42,31 @@ function Player2(player) {
   this.playerName = player;
   this.score = 0;
 };
+var diceNumberTemp = ("");
 
-function rollDice(){
-    var die1 = document.getElementById("die1");
-
-    var status = document.getElementById("status");
-    var d1 = Math.floor(Math.random() * 6) + 1;
-
-    var diceTotal = d1;
-    die1.innerHTML = d1;
-
-    status.innerHTML = "You rolled "+diceTotal+".";
-}
+// function rollDice(){
+//     var die1 = document.getElementById("die1");
 //
+//     var status = document.getElementById("status");
+//     var d1 = Math.floor(Math.random() * 6) + 1;
+//
+//     var diceTotal = d1;
+//     die1.innerHTML = d1;
+//
+//     status.innerHTML = "You rolled "+diceTotal+".";
+//
+//     diceNumberTemp.concat(d1);
+// }
+
+// function diceNumberTemp(index) {
+//   if (index=== 1){
+//     alert("your turn is over");
+//   }else{
+//     alert("plese roll again");
+//   }
+// };
+// alert(diceNumberTemp);
+// //
 //  Player.prototype.increaseScore = function(diceRoll) {
 //    this.score += diceRoll;
 // };
